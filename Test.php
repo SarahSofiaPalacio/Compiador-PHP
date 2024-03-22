f1e835b3d005790dab6812c9f9a98773f33685c1
SarahSofiaPalacio <79477472+SarahSofiaPalacio@users.noreply.github.com>
2024-03-22 14:39:04 -0500

    Lexer y test

---
+++ Test.php
@@ -0,0 +1,35 @@
+<?php
+// Comentario de una sola linea
+# Comentario de una sola linea
+
+ /* Comentario 
+ de varias 
+ lineas
+
+ */
+include 'studio';
+include "studio";
+const prueba="hola";
+const prueba=213;
+print("Hola mundo como en los viejo tiempos");
+$F = 5;
+<var> $X = 0 </var>;
+while ($X < $F) {
+    $X = $X + 1;
+}
+echo ("\n El valor de X es: $X");
+$_hola = 3;
+$hola1 = 3, $hola2 = 3;
+$lista = array("dsda",3,4,5);
+$lista1 = array($F,$hola,$lista,);
+print $hola;
+
+class SimpleClass
+{
+    // property declaration
+    $var = 2;
+
+}
+$var = new SimpleClass();
+
+?>
