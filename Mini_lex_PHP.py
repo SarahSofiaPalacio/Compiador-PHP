6a737a87e53a79732f101b695db4c7b97737017c
Darkian69 <99846711+Darkian69@users.noreply.github.com>
2024-03-25 23:55:57 -0500

    Update Mini_lex_PHP.py
    Agregue que ahora detecta todo el comentario y otras

--- Mini_lex_PHP.py
+++ Mini_lex_PHP.py
@@ -14,6 +14,8 @@ reserved = {
     'CATCH': 'catch',
     'CLASS': 'class',
     'CLONE': 'clone',
+	'OBJECTOPERATOR' : 'objectoperator',
+    'READONLY': 'readonly',
     'CONST': 'const',
     'CONTINUE': 'continue',
     'DECLARE': 'declare',
@@ -76,16 +78,25 @@ reserved = {
 
 tokens = [
     #SYMBOLS
+	'OPEN_TAG',
+    'CLOSE_TAG',
     'ASSIGN',
+	'MULEQUAL',
     'MOD',
     'PLUS',
+	'BACKSLASH',
     'PLUSPLUS',
     'PLUSEQUAL',
     'MINUS',
     'MINUSMINUS',
     'MINUSEQUAL',
+	'COMMENT',
+	'COMMENT_MULTI',
     'TIMES',
     'DIVIDE',
+	'DIVEQUAL',
+    'XOREQUAL',
+    'POW',
     'LESS',
     'LESSEQUAL',
     'GREATER',
@@ -94,7 +105,17 @@ tokens = [
     'DEQUAL',
     'DISTINT',
     'ISEQUAL',
+	'ISIDENTICAL',
+    'ISNOTIDENTICAL',
+    'BOOL_OR',
+    'BOOL_AND',
+    'ANDEQUAL',
     'SEMICOLON',
+	'SL',
+    'SLEQUAL',
+    'SR',
+    'SREQUAL',
+    'NOT',
     'COMMA',
     'LPAREN',
     'RPAREN',
@@ -109,7 +130,7 @@ tokens = [
     'QUESTIONMARK',
     'COMILLASIMPLE',
     'COMILLASDOBLES',
-
+    'COMMENT_HASHTAG',
     'VARIABLE', 
     'VARIABLE2', 
     'NUMBER',
@@ -125,6 +146,7 @@ t_PLUS   = r'\+'
 t_MINUS  = r'-'
 t_TIMES  = r'\*'
 t_DIVIDE = r'/'
+#t_BACKSLASH = r'\\',
 t_EQUAL  = r'='
 t_DISTINT = r'!'
 t_LESS   = r'<'
@@ -146,7 +168,13 @@ t_COMILLASDOBLES = r'\"'
 t_QUESTIONMARK = r'\?'
 
 
+def t_OPEN_TAG(t): 
+    r'<\?php'
+    return t
 
+def t_CLOSE_TAG(t): 
+    r'\?>'
+    return t
  
 def t_NUMBER(t):
     r'\d+(\.\d+)?'
@@ -157,7 +185,7 @@ def t_NUMBER(t):
 def t_VARIABLE(t):
     r'\$[a-zA-Z_](\w)*'
     return t
-""""
+"""
 def t_VARIABLE2(t):
     r'[a-zA-Z](\w)*'
     if t.value in tokens:
@@ -165,6 +193,23 @@ def t_VARIABLE2(t):
         return t
     else:
         return t """
+
+def t_COMMENT(t):
+    r'\/\/.*'
+    t.value = t.value  # Establecer el valor del token como el contenido del comentario
+    return t
+
+def t_COMMENT_HASHTAG(t):
+    r'\#.*'
+    t.value = t.value  # Establecer el valor del token como el contenido del comentario
+    return t
+
+# Expresión regular para comentarios de varias líneas
+def t_COMMENT_MULTI(t):
+    r'\/\*(.|\n)*?\*\/'
+    t.value = t.value  # Establecer el valor del token como el contenido del comentario
+    t.lexer.lineno += t.value.count('\n')  # Actualizar el contador de líneas
+    return t
 
 
 
@@ -207,6 +252,72 @@ def t_MINUSMINUS(t):
 	r'--'
 	return t
 
+def t_ANDEQUAL(t): 
+    r'\&=' 
+    return t
+
+def t_ISIDENTICAL(t):
+	r'==='
+	return t
+
+def t_ISNOTIDENTICAL(t):
+	r'!=='
+	return t
+
+def t_BOOL_AND(t): 
+    r'\&\&' 
+    return t
+
+def t_BOOL_OR(t): 
+    r'\|\|' 
+    return t
+
+def t_PLUSEQUAL(t):
+	r'\+='
+	return t
+
+def t_MINUSEQUAL(t):
+	r'-='
+	return t
+
+def t_MULEQUAL(t):
+	r'\*='
+	return t
+
+def t_DIVEQUAL(t):
+	r'/='
+	return t
+
+def t_POW(t):
+	r'\*\*'
+	return t
+
+
+
+def t_BACKSLASH(t):
+    r'\\'
+    return t
+
+def t_XOREQUAL(t): 
+    r'\^=' 
+    return t
+
+def t_SL(t):
+	r'<<'
+	return t
+
+def t_SLEQUAL(t):
+	r'<<='
+	return t
+
+def t_SR(t):
+	r'>>='
+	return t
+
+def t_SREQUAL(t):
+	r'>>='
+	return t
+
 def t_PLUSPLUS(t):
 	r'\+\+'
 	return t
@@ -214,6 +325,7 @@ def t_PLUSPLUS(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+ 
 
 def t_space(t):
     r'\s+'
@@ -224,6 +336,8 @@ t_ignore = ' \t'
 def t_comments(t):
     r'/\*(.|\n)*?\*/'
     t.lexer.lineno += t.value.count('\n')
+	
+
 
 def t_comments_C99(t):
     r'//(.)*?\n'
